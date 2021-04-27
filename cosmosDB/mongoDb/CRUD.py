57228fe974a22451fb686dd6eea49273d0adb354
AdityaJsr <adityajsr1996@gmail.com>
2021-04-27 12:00:03 +0530

    updates

--- cosmosDB/mongoDb/CRUD.py
+++ cosmosDB/mongoDb/CRUD.py
@@ -9,29 +9,37 @@ mycol = mydb[config("mycol")]
 
 # Insert data:
 def user_input():
-    print("I am working")
-    nameid = input("Enter the name id of the person : ")
+    id = input("Enter the unique id : ")
     name = input("Enter the name of the person : ")
     age = input("Enter the age of the person : ")
-    return(nameid,name,age)
+    email_id = input("Enter the email_id of the person : ")
+    return(id,name,age,email_id)
 
 def insert():
-    nameid, name , age = user_input()
-    name = {
-        "nameid" : nameid,
+    id, name , age, email_id,  = user_input()
+    names = {
+        "_id" : id,
         "name" : name,
-        "age" : age
+        "age" : age,
+        "email_id" : email_id
     }
-    mycol.insert_one(name)
+    mycol.insert_one(names)
 
 def display():
     print("Loading Please wait....")
     for items in mycol.find():
         print(items)
+        # for item in items:
+            # print(type(item))
+            # id1 = id,
+            # name1 = name,
+            # age1 = age,
+            # email_id1 = email_id
+            # print("id = " ,id1, "name = " ,name1, "age = " ,age1, "email_id = " ,email_id1,"\n")
 
 
 def main():
-    ch = input("\nEnter the operation you want to perform \n1) Display data :  \n2) Insert data : \n")
+    ch = input("\nEnter the operation you want to perform \n1) Display data :  \n2) Insert data : \nEnter your choice here : ")
     if ch == "1":
         display()
     elif ch == "2":
