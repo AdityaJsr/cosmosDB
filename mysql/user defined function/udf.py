26dd4145368c792c7f6e2dc142ce3c1be5a8b9ab
AdityaJsr <adityajsr1996@gmail.com>
2021-04-29 23:51:36 +0530

    headder added

--- mysql/user defined function/udf.py
+++ mysql/user defined function/udf.py
@@ -1,11 +1,24 @@
+"""
+Title - Write a Python program to create a user defined function in by connecting the age.js file.
+Author name - Aditya Kumar
+Ceation time - 26 April ‎2021 ‏‎
+Modified time - 29 ‎April ‎2021‎
+
+"""
+
+#All the imports required to run the script.
 import azure.cosmos.cosmos_client as cosmos_client
 from decouple import config
 import json
+
+#Credentials kept in a secure .env file.
 
 url = config('url')
 key = config('key')
 database_name = config('database_name')
 container_name = config('container_name')
+
+#Using context manager to open the .JS file
 
 with open('D:/vs studio/bridgeLabz/week 8/mysql/user defined function/age.js') as file:
     file_contents = file.read()
@@ -13,6 +26,8 @@ udf_definition = {
     'id': 'name',
     'serverScript': file_contents,
 }
+#Azure commands to commit the UDFs.
+
 client = cosmos_client.CosmosClient(url, key)
 database = client.get_database_client(database_name)
 container = database.get_container_client(container_name)
