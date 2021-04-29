031e026375c44b306cfad78f54f856d46c05b431
AdityaJsr <adityajsr1996@gmail.com>
2021-04-29 12:46:14 +0530

    age.js file added

---
+++ mysql/user defined function/age.js
@@ -0,0 +1,13 @@
+
+function userDefinedFunction(name){
+    if(age == undefined)
+        throw "age not defined";
+
+    if (age < 50)
+        return ('under 50');
+    else if (age > 100)
+        return ('above 50');
+    else 
+        return 'undefined';            
+
+}
