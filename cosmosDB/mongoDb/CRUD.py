38a728fdee4423d7042b53ab90dd640b1f975c33
AdityaJsr <adityajsr1996@gmail.com>
2021-04-30 00:02:54 +0530

    Headder added with dockstring

--- cosmosDB/mongoDb/CRUD.py
+++ cosmosDB/mongoDb/CRUD.py
@@ -1,6 +1,15 @@
+"""
+Title - Write a Python program to to perform CRUD operations on the mongodb api (azure).
+Author name - Aditya Kumar
+Ceation time - 23 April ‎2021 ‏‎
+Modified time - 29 ‎April ‎2021‎
+
+""" 
+#All the imports required to run the script.
 import pymongo
 from decouple import config
 
+# Connection to azure mongodb API.
 try:
 
     client = pymongo.MongoClient(config("uri"))
@@ -10,8 +19,17 @@ try:
     mycol = mydb[config("mycol")]
 except Exception as e:
     print("Could not connect to the database", e)
-# Insert data:
 
+'''
+Function:
+        The function user_input() is to take user input to add items in DB.
+Arguments:
+        none
+Returns:
+        id,name,age,email_id : These items are returned to the insert() function.
+Errors:
+        none.
+'''
 def user_input():
     try:
         id = input("Enter the unique id : ")
@@ -21,6 +39,17 @@ def user_input():
         return(id,name,age,email_id)
     except Exception as e:
         print("Invalid inputs")
+
+'''
+Function:
+        The function insert() is to commit user input to the container in DB.
+Arguments:
+        user_input() :  The user input function is called to take input for new entry.
+Returns:
+        none.
+Errors:
+        none.
+'''
 def insert():
     try:
         id, name , age, email_id,  = user_input()
@@ -33,6 +62,17 @@ def insert():
         mycol.insert_one(names)
     except Exception as e:
         print("An exception has occured", e)
+
+'''
+Function:
+        The function display() is to depict data retrived from mongo DB.
+Arguments:
+        none.
+Returns:
+        none.
+Errors:
+        none.
+'''
 
 def display():
     try:
@@ -54,7 +94,16 @@ def delete():
         print(deleted_val.deleted_count, "Deleted document successfull")
     except Exception as e:
         print("Exception occured as", e)
-
+'''
+Function:
+        The function update() is to update a single item identified byuser input to the container in DB.
+Arguments:
+        none.
+Returns:
+        none.
+Errors:
+        none.
+'''
 def update():
     try:
         email_id = input("Enter the email id of the user you want to update : ")
@@ -65,7 +114,16 @@ def update():
         print(update_val, "Updated document successfull")
     except Exception as e:
         print("Exception occured as", e)
-
+'''
+Function:
+        The function find() is to a specific single value from mongo DB.
+Arguments:
+        none.
+Returns:
+        none.
+Errors:
+        none.
+'''
 def find():
     try:
         email_id = input("Enter the email id of the user you want to find : ")
@@ -75,6 +133,16 @@ def find():
     except Exception as e:
         print("Exception occured as", e)
 
+'''
+Function:
+        The function main() is the main file for CRUD.py.
+Arguments:
+        none.
+Returns:
+        none.
+Errors:
+        none.
+'''
 
 def main():
     try:
